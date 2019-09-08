30ca5ad1b14304595541ad580b9977ca1bc3a614
Bava <loic.steinmetz2@gmail.com>
2019-09-08 18:06:05 +0200

    add contact_email validations

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -1,6 +1,43 @@
 class MailerController < ApplicationController
+  before_action :validations, only: [:contact_email]
+
   def contact_email
     ContactMailer.contact_email.deliver_now
     redirect_to root_path
+  end
+
+  private
+
+  def validations
+    errors = []
+    errors << 'Prénom manquant' unless first_name?
+    errors << 'Nom manquant' unless last_name?
+    errors << 'Email ou confirmation d\'email manquant' unless email?
+    errors << 'Les adresses emails ne concordent pas' unless valid_email?
+    errors << 'Message vide' unless message?
+    unless errors.empty?
+      flash[:alert] = errors
+      redirect_to '/home#contact'
+    end
+  end
+
+  def first_name?
+    !params[:first_name].empty? ? true : false
+  end
+
+  def last_name?
+    !params[:last_name].empty? ? true : false
+  end
+
+  def email?
+    !params[:email].empty? && !params[:email_validation].empty? ? true : false
+  end
+
+  def valid_email?
+    params[:email] == params[:email_validation] ? true : false
+  end
+
+  def message?
+    !params[:message].empty? ? true : false
   end
 end
--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,2 +1,3 @@
 module MailerHelper
+  
 end
