649d8f037fb3cef6e86eb21254ad383b4211c699
Bava <loic.steinmetz2@gmail.com>
2019-09-09 09:28:10 +0200

    fix alerts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # LetterOpener config
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 end
