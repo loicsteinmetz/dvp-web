e85b1a1045c343874ec80850ffedf075b1affb65
Bava <loic.steinmetz2@gmail.com>
2019-09-09 09:42:51 +0200

    add scrolling for nav links

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -2,12 +2,25 @@ $(document).ready(function(){
 
   let str = 'portfolio';
   let url = document.documentURI;
+
+  // links:hover
   let home = document.querySelectorAll('.nav-item a')[0];
   let portfolio = document.querySelectorAll('.nav-item a')[1];
   if (url.includes(str)){
     portfolio.classList.add('on')
   } else {
     home.classList.add('on')
+  };
+
+  // scrolling for nav links
+  if (!(url.includes(str))){
+    let scrollLink = $('.nav-link');
+    scrollLink.click(function (e) {
+      e.preventDefault();
+      $('body,html').animate({
+          scrollTop: $(this.hash).offset().top
+      })
+  })
   };
   
 });
