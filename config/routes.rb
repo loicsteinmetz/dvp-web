d0a99c0c037934629fb34384a3587b29c971c3d1
Bava <loic.steinmetz2@gmail.com>
2019-09-16 12:43:51 +0200

    init admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # get '/portfolio', to: 'static#portfolio'
   get '/mentions_legales', to: 'static#mentions_legales'
   post '/mail', to: 'mailer#contact_email'
+  namespace :admin do
+    root to: 'admin#dashboard'
+    resources :contents, only: [:update]
+    resources :folios, only: [:update]
+    resources :cv_entries, only: [:update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
