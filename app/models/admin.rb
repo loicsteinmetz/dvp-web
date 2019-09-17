91e0a67056322dd068ed50cdc8c17c10e0e8540b
Bava <loic.steinmetz2@gmail.com>
2019-09-17 14:03:09 +0200

    remove not used devise routes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,5 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 end
