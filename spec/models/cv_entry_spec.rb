fd078b0f4edeac39ee72ed34e0e2a85fd47e7c7f
Bava <loic.steinmetz2@gmail.com>
2019-09-18 08:45:46 +0200

    upt cv_entry model tests

--- spec/models/cv_entry_spec.rb
+++ spec/models/cv_entry_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe CvEntry, type: :model do
 
   before(:each) do 
-    @entry = CvEntry.create(time: "depuis 2019", title: "DUT informatique", sub: "IUT Nancy Charlemagne", order: 0)
+    @time = TimeCv.create(date:'2012')
+    @entry = CvEntry.create(date: @time, title: "DUT informatique", sub: "IUT Nancy Charlemagne", order: 0)
   end
 
   context "validations" do
@@ -13,28 +14,28 @@ RSpec.describe CvEntry, type: :model do
       expect(@entry).to be_valid
     end
     describe "time" do
-      it "should not be valid without time" do
+      it "should not be valid without date" do
         bad_entry = CvEntry.create(title: "DUT informatique", sub: "IUT Nancy Charlemagne", order: 0)
         expect(bad_entry).not_to be_valid
-        expect(bad_entry.errors.include?(:time)).to eq(true)
+        expect(bad_entry.errors.include?(:date)).to eq(true)
       end
     end
     describe "title" do
       it "should not be valid without title" do
-        bad_entry = CvEntry.create(time: "depuis 2019", sub: "IUT Nancy Charlemagne", order: 0)
+        bad_entry = CvEntry.create(date: @time, sub: "IUT Nancy Charlemagne", order: 0)
         expect(bad_entry).not_to be_valid
         expect(bad_entry.errors.include?(:title)).to eq(true)
       end
       it "should not be valid with a title which is not unique" do
-        CvEntry.create(title: "DUT informatique", time: "depuis 2019", sub: "IUT Nancy Charlemagne", order: 0)
-        bad_entry = CvEntry.create(title: "DUT informatique", time: "depuis 2019", sub: "IUT Nancy Charlemagne", order: 0)
+        CvEntry.create(title: "DUT informatique", date: @time, sub: "IUT Nancy Charlemagne", order: 0)
+        bad_entry = CvEntry.create(title: "DUT informatique", date: @time, sub: "IUT Nancy Charlemagne", order: 0)
         expect(bad_entry).not_to be_valid
         expect(bad_entry.errors.include?(:title)).to eq(true)
       end
     end
     describe "order" do
       it "should not be valid without order" do
-        bad_entry = CvEntry.create(title: "DUT informatique", time: "depuis 2019", sub: "IUT Nancy Charlemagne")
+        bad_entry = CvEntry.create(title: "DUT informatique", date: @time, sub: "IUT Nancy Charlemagne")
         expect(bad_entry).not_to be_valid
         expect(bad_entry.errors.include?(:order)).to eq(true)
       end
@@ -52,12 +53,12 @@ RSpec.describe CvEntry, type: :model do
         expect(@entry.title).to eq("DUT informatique")
       end
     end
-    describe "CvEntry.time" do
-      it 'should return a string' do
-        expect(@entry.time).to be_a(String)
+    describe "CvEntry.date" do
+      it 'should return a TimeCv object' do
+        expect(@entry.date).to be_a(TimeCv)
       end
-      it 'should return the time' do
-        expect(@entry.time).to eq("depuis 2019")
+      it 'should return the date category' do
+        expect(@entry.date).to eq(@time)
       end
     end
     describe "CvEntry.sub" do
