fe6817125d2dcb9801ca44ee102b1770de32ad8a
Bava <loic.steinmetz2@gmail.com>
2019-09-18 09:03:01 +0200

    add test for admin controller

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+
+  before(:each) do 
+    @admin = Admin.create(email: 'email@email.com', password: 'testestest')
+  end
+
+  context "validations" do
+
+    it "is valid with valid attributes" do
+      expect(@admin).to be_a(Admin)
+      expect(@admin).to be_valid
+    end
+
+  end
+
+  context "public instance methods" do
+
+    describe "Admin.email" do
+      it 'should return a string' do
+        expect(@admin.email).to be_a(String)
+      end
+      it 'should return the email' do
+        expect(@admin.email).to eq('email@email.com')
+      end
+    end
+    describe "Admin.encrypted_password" do
+      it 'should return a string' do
+        expect(@admin.encrypted_password).to be_a(String)
+      end
+      it 'should return an encrypted password' do
+        expect(@admin.encrypted_password).not_to eql('testestest')
+        expect(@admin.encrypted_password.size).to be > 20
+      end
+    end
+
+  end
+
+end
