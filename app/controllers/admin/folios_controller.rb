bdff1bd53e1a2b76e05a281c584cdfbae2194673
Bava <loic.steinmetz2@gmail.com>
2019-09-24 18:53:12 +0200

    add folios# edit, update, destroy

--- app/controllers/admin/folios_controller.rb
+++ app/controllers/admin/folios_controller.rb
@@ -22,12 +22,32 @@ class Admin::FoliosController < ApplicationController
   end
 
   def edit
+    @folio = Folio.find(params[:id])
+    respond_to do |format|
+      format.js {}
+    end
   end
 
   def update
+    @folio = Folio.find(params[:id])
+    update = params.permit(:title, :website, :description, :description_mini, :techs, :work)
+    @folio.capture.attach(params[:capture]) if !params[:capture].nil?
+    if @folio.update(update) || !params[:capture].nil?
+      @valid = true
+    else
+      flash[:alert] = 'Error'
+    end
+    respond_to do |format|
+      format.js {}
+    end
   end
 
   def destroy
+    @folio = Folio.find(params[:id])
+    @folio.destroy
+    respond_to do |format|
+      format.js {}
+    end
   end
 
   def order_up
