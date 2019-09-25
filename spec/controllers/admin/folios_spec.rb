62511988d1a7080e37678a989925207057f26584
Bava <loic.steinmetz2@gmail.com>
2019-09-25 08:08:10 +0200

    upd folio controller spec

--- spec/controllers/admin/folios_spec.rb
+++ spec/controllers/admin/folios_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe Admin::FoliosController, type: :controller do
     folio.save
     return folio
   end
+  let(:folio2) do
+    folio = Folio.new(title: "website", website: "https://website.com", work: "Lorem ipsum dolor sit amet", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu. Lacinia quis vel eros donec. Non odio euismod lacinia at quis risus. Eget nulla facilisi etiam dignissim diam quis enim lobortis. Ut diam quam nulla porttitor massa id. Egestas dui id ornare arcu odio. Nisl nisi scelerisque eu ultrices vitae auctor. Fermentum iaculis eu non diam phasellus vestibulum lorem.", techs: "Ruby, JS, Html, Rails, etc...", description_mini: "Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+    folio.capture.attach(io: open("http://placeimg.com/600/400/any"), filename: 'capture.jpeg')
+    folio.save
+    return folio
+  end
 
   describe "create" do
     it "redirects to login page if no admin auth" do
@@ -97,6 +103,47 @@ RSpec.describe Admin::FoliosController, type: :controller do
       sign_in admin
       delete :destroy, params: { id: f.id }, format: :js, xhr: true
       expect(CvEntry.count).to eq(0)
+    end
+  end
+
+  describe 'reorder up and down' do
+    it "redirects to login page if no admin auth" do
+      f = folio
+      f2 = folio2
+      post :order_up, params: { id: f.id, format: 'js' }
+      expect(response.status).to eq(401)
+    end
+    it 'reorders up' do
+      f = folio
+      f2 = folio2
+      sign_in admin
+      post :order_up, params: { id: f2.id, format: 'js' }
+      expect(Folio.last.order).to eq(0)
+      expect(Folio.first.order).to eq(1)
+    end
+    it 'does not reorder up for a first position instance' do
+      f = folio
+      f2 = folio2
+      sign_in admin
+      post :order_up, params: { id: f.id, format: 'js' }
+      expect(Folio.last.order).to eq(1)
+      expect(Folio.first.order).to eq(0)
+    end
+    it 'reorders down' do
+      f = folio
+      f2 = folio2
+      sign_in admin
+      post :order_down, params: { id: f.id, format: 'js' }
+      expect(Folio.last.order).to eq(0)
+      expect(Folio.first.order).to eq(1)
+    end
+    it 'does not reorder down for a last position instance' do
+      f = folio
+      f2 = folio2
+      sign_in admin
+      post :order_down, params: { id: f2.id, format: 'js' }
+      expect(Folio.last.order).to eq(1)
+      expect(Folio.first.order).to eq(0)
     end
   end
 
